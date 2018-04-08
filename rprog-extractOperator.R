e51559774d039de577ffb2176c2e7f27872b8947
Leonard Greski <len@ageretechnology.com>
2018-04-08 10:06:10 -0700

    add code to download Pokemon files and replicate analysis for Assignment 1 portion of Forms of the Extract Operator.

--- rprog-extractOperator.R
+++ rprog-extractOperator.R
@@ -2,4 +2,18 @@
 # R code for Forms of the Extract Operator article
 #
 
-# stored in datascience repository rprogramming folder 
+# stored in datasciencectacontent
+
+download.file("https://github.com/lgreski/PokemonData/raw/master/pokemonData.zip","pokemonData.zip",
+               mode="wb",method="wininet")
+
+unzip("pokemonData.zip")
+
+thePokemonFiles <- list.files("./pokemonData",
+                              full.names=TRUE)[1:2]
+thePokemonFiles
+
+pokemonData <- lapply(thePokemonFiles,function(x) read.csv(x)[["Attack"]])
+
+# show the list of vectors
+summary(pokemonData)
